db2a7afb83a9bcc712597573c22b1000701cacc3
Deep210-dot <64728675+Deep210-dot@users.noreply.github.com>
2021-09-13 11:32:58 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
 
-const alchemy_api_key ="Paste your App Key From Alchemy Account";  //Make sure your aap is connected with the stated test network
+const alchemy_api_key ="Paste your App Key From Alchemy Account";  //Make sure your app is connected with the stated test network
 const metamask_Private_Key ="Paste your Metamask Private key Here"; //Make sure your metamask is connected with the stated test network
 
 module.exports = {
