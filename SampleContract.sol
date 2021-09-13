d20ce3a762ff7ae60455b1da356b693671df1748
Deep210-dot <64728675+Deep210-dot@users.noreply.github.com>
2021-09-13 11:28:22 +0530

    Add files via upload

--- SampleContract.sol
+++ SampleContract.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+contract Token{
+string public name = "Quanta";
+string public symbol ='Q+';
+uint public totalSupply = 10000;
+address public founder;
+mapping(address=>uint) balances;
+
+constructor() {
+    balances[msg.sender] = totalSupply;
+    founder = msg.sender;
+}
+function transfer(address _to, uint _amount) public{
+    require(balances[msg.sender]>=_amount,"Not Enough Tokens");
+    balances [msg.sender] -= _amount;
+    balances[_to] += _amount;
+}
+function balanceOf(address _account) external view returns(uint){
+    return balances[_account];
+}
+}
 
